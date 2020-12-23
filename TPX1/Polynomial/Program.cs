9784f343b513a7edaee65bcdf3f6ec125f034373
Augustin Bégué <augustin.begue@epita.fr>
2020-12-23 15:12:56 +0100

    Prepared functions for the polynomial editor

--- TPX1/Polynomial/Program.cs
+++ TPX1/Polynomial/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Polynomial
 {
@@ -10,7 +11,7 @@ namespace Polynomial
 
         public static float ComputeCovidCases(float day)
         {
-            float[] coeffs = new[] { 0.6f, -2.133257f, 2.182121f, -0.399069f, 0.029503f, -0.000803f };
+            float[] coeffs = new[] {0.6f, -2.133257f, 2.182121f, -0.399069f, 0.029503f, -0.000803f};
 
             float res = 0f;
             for (int i = 0; i < coeffs.Length; i++)
@@ -20,7 +21,64 @@ namespace Polynomial
 
             return res;
         }
-        
+
+        public static float[] AddCoefficient(float[] coefficients, float x, int exponent)
+        {
+            if (exponent >= coefficients.Length)
+            {
+                float[] newCoefficients = new float[exponent+1];
+
+                for (int i = 0; i < coefficients.Length; i++)
+                {
+                    newCoefficients[i] = coefficients[i];
+                }
+
+                newCoefficients[exponent] = x;
+
+                return newCoefficients;
+            }
+            else
+            {
+                coefficients[exponent] = x;
+                return coefficients;
+            }
+        }
+
+        public static string PolynomialToString(float[] coefficients)
+        {
+            string res = "";
+
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                var coeff = coefficients[i];
+
+                if (coeff != 0f)
+                {
+                    string sign = " + ";
+                    
+                    if (coeff < 0)
+                        sign = " - ";
+
+                    if (i == 0)
+                    {
+                        if (coeff < 0)
+                            sign = "- ";
+                        else
+                            sign = "- ";
+                    }
+                        
+
+                    res += $"{sign}{Math.Abs(coeff)}";
+
+                    if (i != 0)
+                        res += $"^{i}";
+                    
+                }
+            }
+
+            return res;
+        }
+
         public static float ComputePolynomial(float x, float[] coefficients)
         {
             float res = 0f;
